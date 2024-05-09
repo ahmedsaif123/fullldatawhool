202cb0ad613a6bdc8a07f00e8ee1f03d22ae3820
ahmedsaif123 <ahmedabnsaif@gmail.com>
2024-05-09 15:53:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ gameOn.addEventListener("click", () => {
     } else if (clickCount == 3) {
         finalValue.innerHTML = `
         <img src='${displayRandomImage()}' style="width:235px; height:235px; margin-top: 3%;" alt="" id="photo">
-        <button style='background:#000;'> <a href='facebook.com'>مبروك كسبت معانا</a></button>
+        <button style='background:#000;'> <a href='https://2u.pw/D9ZZn4au'>مبروك كسبت معانا</a></button>
 
         `;
     }
